3f498e7c99e27919625cfb356e3851c4a7f0d64b
hussainkmajmal786-sketch <hussainkmajmal786@gmail.com>
2026-01-19 11:15:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+  return {
+    base: "/my-site/",
+    server: {
+      port: 3000,
+      host: "0.0.0.0",
+    },
+    plugins: [react()],
+    ...
